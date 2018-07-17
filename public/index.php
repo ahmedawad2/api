d92040b27dfefbeb56456e254feede12d3be4f32
ahmed@ubuntu <ahmed.awad@edigits.net>
2018-07-17 13:33:51 +0200

    adding post request + different ways to get GET data

--- public/index.php
+++ public/index.php
@@ -5,10 +5,44 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
 
-    return $response;
+//$app->get('/hello/{fname}/{lname}', function (Request $request, Response $response, array $args) {
+//    $fname = $args['fname'];
+//    $lname = $args['lname'];
+//    $third = $request->getQueryParams()['third'];
+//    $response->getBody()->write("Hello, ($fname $lname), the 3rd param is $third");
+//
+//    return $response;
+//});
+
+
+//$app->any('/post', function(Request $req, Response $res){
+//    $data = $req->getParsedBody();
+//    $name = $data['name'];
+//    $phone = $data['phone'];
+//    $res->getBody()->write("welcome $name, your phone is : $phone");
+////    return $res;
+//
+//});
+
+
+
+$app->get('/hello/{fname}/{lname}', function(Request $req, Response $res, array $args){
+    $fname = $req->getAttribute('fname');
+    $lname = $args['lname'];
+    $third = $req->getQueryParams()['third'];
+
+    $res->getBody()->write("hello ($fname $lname), the third parameter is: $third");
+    return $res;
 });
+
+$app->post('/testPost', function(Request $req, Response $res){
+    $data = $req->getParsedBody();
+    $name = $data['name'];
+    $phone = $data['phone'];
+
+    $res->getBody()->write("welcomme $name, your phone is: $phone");
+});
+
+
 $app->run();
